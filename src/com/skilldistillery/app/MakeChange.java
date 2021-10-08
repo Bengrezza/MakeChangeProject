99730c800bc29b2fe213cea05b2e8d54d606d97b
Bengrezza <bengretza@gmail.com>
2021-10-08 16:20:59 -0600

    create menu method

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,9 +1,12 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(system.in);
+		
 
 	}
 
